e1e0fad97933de2aba1167dbc650bd4a9dca6172
Kian Candelario <kiancandelario1@gmail.com>
2023-11-07 18:26:30 +0800

    initial commit

---
+++ web_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.write("""
+
+# Salary Predictor
+### Short Description: 
+This project uses *Random Forest Regression*— a machine learning algorithm— to predict the potential salary of a person. It uses a dataset that contains data such as  *Age, Gender, Education level, Job title, and Years of Experience*.
+         
+""")
