fbb7b44ae842e6e1b5754d58192a9e4370e7d033
Kian Candelario <kiancandelario1@gmail.com>
2023-11-07 21:42:22 +0800

    feat: Added a brief introduction

--- web_app.py
+++ web_app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 
-st.write("""
+st.set_page_config(page_title="", page_icon="", layout="wide")
 
-# Salary Predictor
-### Short Description: 
-This project uses *Random Forest Regression*— a machine learning algorithm— to predict the potential salary of a person. It uses a dataset that contains data such as  *Age, Gender, Education level, Job title, and Years of Experience*.
-         
-""")
+# Title container (brief introduction of the project)
+with st.container():
+    st.title("Salary Predictor")
+
+    st.subheader("Short description")
+    st.write("This project uses *Random Forest Regressor*— a machine learning regression algorithm— to predict the potential salary of a person. It uses a dataset that contains data such as  *Age, Gender, Education level, Job title, and Years of Experience*.")
+    st.write("**NOTE:** The dataset used in this project was **large language models generated** and **not collected from actual data sources**. To learn more about the dataset, [click here](https://www.kaggle.com/datasets/rkiattisak/salaly-prediction-for-beginer).")
+
