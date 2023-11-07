5c43d256e79e3b2d6e0a9e4664ff15c8f52d4417
Kian Candelario <kiancandelario1@gmail.com>
2023-11-08 00:41:20 +0800

    update: Finished the title section

--- web_app.py
+++ web_app.py
@@ -1,12 +1,34 @@
 import streamlit as st
+from streamlit_lottie import st_lottie
 
-st.set_page_config(page_title="", page_icon="", layout="wide")
+# ASSETS
+animation = "https://lottie.host/dedcb31b-6506-43a2-a505-bf43a8042b69/2Q9BXeIUuV.json"
 
-# Title container (brief introduction of the project)
+
+
+# --- Setting the page configuration
+st.set_page_config(page_title="Salary Predictor", page_icon="", layout="wide")
+
+# --- Using CSS to hide the Streamlit Branding
+def css(filename):
+    with open(filename) as file:
+        st.markdown(f"<style>{file.read()}</style>", unsafe_allow_html=True)
+css('./styles/styles.css')
+
+
+
+# --- Title section (brief introduction of the project)
 with st.container():
-    st.title("Salary Predictor")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.title("Salary Predictor :money_with_wings:")
 
-    st.subheader("Short description")
-    st.write("This project uses *Random Forest Regressor*— a machine learning regression algorithm— to predict the potential salary of a person. It uses a dataset that contains data such as  *Age, Gender, Education level, Job title, and Years of Experience*.")
-    st.write("**NOTE:** The dataset used in this project was **large language models generated** and **not collected from actual data sources**. To learn more about the dataset, [click here](https://www.kaggle.com/datasets/rkiattisak/salaly-prediction-for-beginer).")
+        st.subheader("Short description")
+        st.write("This project uses *Random Forest Regressor*— a machine learning regression algorithm— to predict the potential salary of a person. It uses a dataset that contains data such as  *Age, Gender, Education level, Job title, and Years of Experience*.")
+        st.write("**NOTE:** The dataset used in this project was **large language models generated** and **not collected from actual data sources**. To learn more about the dataset, [click here](https://www.kaggle.com/datasets/rkiattisak/salaly-prediction-for-beginer).")
+    with right_column:
+        st_lottie(animation, height=350, key="salary")
 
+# --- Main content section
+with st.container():
+    st.write("---")
